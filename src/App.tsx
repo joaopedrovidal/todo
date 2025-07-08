8960383886acaae1f72008d3a2660127792af1ce
Joao Pedro <ppvidalsantos@gmail.com>
2025-07-07 23:57:41 -0300

    Componente Text

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
+import Text from "./components/text";
+
 export default function App() {
 
   return (
-    <div className="text-pink-dark">
-      Olá mundo!
+    <div className="flex flex-col gap-2">
+      <Text variant={"body-sm-bold"} className="text-pink-base">
+        Olá mundo!
+      </Text>
+      <Text className="text-green-base">
+        Olá Mundo
+      </Text>
+      <Text variant={"body-md-bold"}>
+        Olá mundo!
+      </Text>
+      <Text>
+        Levar o dog para passear
+      </Text>
     </div>
   )
 }
