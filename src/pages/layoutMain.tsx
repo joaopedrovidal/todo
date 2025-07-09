733efd7f13dff1cd3923b5906c3eb870b01f8e2f
Joao Pedro <ppvidalsantos@gmail.com>
2025-07-09 00:34:40 -0300

    Estruturando o corpo do Layout

--- src/pages/layoutMain.tsx
+++ src/pages/layoutMain.tsx
@@ -1,26 +1,18 @@
 import { NavLink, Outlet } from "react-router";
 import Container from "../components/Container";
 import Text from "../components/Text";
+import Header from "../core-components/Header";
+import MainContent from "../core-components/MainContent";
+import Footer from "../core-components/Footer";
 
 export default function LayoutMain(){
-    return(
+    return (
         <>
-            <Container as="header" className="mt-3 md:mt-20">
-                Olá mundo - Header
-            </Container>
-            <main className="mt-4 md:mt-8">
+            <Header />
+            <MainContent>
                 <Outlet />
-            </main>
-            <footer className="my-5 md:my-10">
-                <nav className="flex items-center justify-center gap-4">
-                    <NavLink to='/'>
-                        <Text variant={"body-sm-bold"} className="text-gray-300">Tarefas</Text>
-                    </NavLink>
-                    <NavLink to='/componentes'>
-                        <Text variant={"body-sm-bold"} className="text-gray-300">Componentes</Text>
-                    </NavLink>                    
-                </nav>
-            </footer>
+            </MainContent>
+            <Footer />
         </>
     )
 }
